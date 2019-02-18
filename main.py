d1e3ed6d3f4310de160a90f4a326b0f90e2a7cbd
Sebastouf <raph.en223@gmail.com>
2019-02-18 14:27:44 +0100

    Reset soft ptoject
    Merge commits

---
+++ main.py
@@ -0,0 +1,41 @@
+import json
+
+
+def initialization_coord(general_maze, char):
+    """From the maze return the initial position of the runner
+    Args :
+        general_maze: The maze to current maze to perfom
+        char : Char, will distinct the start point from end point
+    Return :
+        A list with x and y as int
+    """
+    for row in general_maze:
+        for column in row:
+            if column == char:
+                return [row.index(column), general_maze.index(row)]
+
+
+def maze_launch(current_maze, start, end):
+    """Will launch one or multiple algorithms to resolve the maze
+    Args :
+        current_maze: List of list, char, the current maze to perform
+        start: List, a vector with the initial start position
+        end : List, a vector with the end position
+    Returns :
+        maze_pathed : The maze with the path to the end
+    """
+    path_maze = []
+    return path_maze
+
+
+if __name__ == "__main__":
+
+    with open('./Grid/grid.json') as grid:
+        maze = json.load(grid)
+
+    start_coords = initialization_coord(maze, "S")
+    end_coords = initialization_coord(maze, "E")
+
+    for line in maze_launch(maze, start_coords, end_coords):
+        print(line)
+    pass
